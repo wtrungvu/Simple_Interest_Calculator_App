9fc8de168f7d4480b4fe8c30657b00743f321f38
Trung Vu <wtrungvu@gmail.com>
2019-08-02 16:26:05 +0700

    Add logical code

--- simple_interest_calculator_app/lib/main.dart
+++ simple_interest_calculator_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_money_formatter/flutter_money_formatter.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +8,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Simple Interest Calculator",
-      theme: ThemeData(accentColor: Colors.green),
+      theme: ThemeData(
+          primaryColor: Colors.blue,
+          accentColor: Colors.blueAccent,
+          brightness: Brightness.dark),
       home: HomeScreen(),
     );
   }
@@ -18,7 +22,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Simple Interest Calculator"),
+        title: Text("Tính lãi suất"),
       ),
       body: HomeScreenWidget(),
     );
@@ -31,10 +35,25 @@ class HomeScreenWidget extends StatefulWidget {
 }
 
 class _HomeScreenWidgetState extends State<HomeScreenWidget> {
-  var _currencies = ["Dollars", "VietNamDong", "Rupees", "Pounds"];
-  var _currentItemSelected = "VietNamDong";
+  var _currencies = ["VietNamDong", "Dollars", "Rupees", "Pounds"];
+  var _currentItemSelected;
+
+  TextEditingController principalController = TextEditingController();
+  TextEditingController roiController = TextEditingController();
+  TextEditingController termController = TextEditingController();
+
+  String displayResult = "";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _currentItemSelected = _currencies[0];
+  }
+
   @override
   Widget build(BuildContext context) {
+    TextStyle textStyle = Theme.of(context).textTheme.title;
     return SingleChildScrollView(
       child: Center(
         child: Container(
@@ -54,27 +73,36 @@ class _HomeScreenWidgetState extends State<HomeScreenWidget> {
               Container(
                 margin: const EdgeInsets.only(top: 50.0),
                 child: TextField(
+                    controller: principalController,
+                    style: textStyle,
+                    keyboardType: TextInputType.number,
                     decoration: new InputDecoration(
-                  labelText: "Principal",
-                  hintText: "Enter pricipal e.g. 12000",
-                  fillColor: Colors.white,
-                  border: new OutlineInputBorder(
-                    borderRadius: new BorderRadius.circular(10.0),
-                    borderSide: new BorderSide(),
-                  ),
-                )),
+                        labelText: "Số tiền gốc ban đầu",
+                        labelStyle: textStyle,
+                        hintText: "Ví dụ: 50000đ",
+                        fillColor: Colors.white,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(10.0),
+                          borderSide: new BorderSide(),
+                        ),
+                        prefixIcon: Icon(Icons.attach_money))),
               ),
               Container(
                 margin: const EdgeInsets.only(top: 20.0),
                 child: TextField(
+                    controller: roiController,
+                    style: textStyle,
+                    keyboardType: TextInputType.number,
                     decoration: new InputDecoration(
-                  labelText: "Rate of Interest",
-                  fillColor: Colors.white,
-                  border: new OutlineInputBorder(
-                    borderRadius: new BorderRadius.circular(10.0),
-                    borderSide: new BorderSide(),
-                  ),
-                )),
+                        labelText: "Số lãi suất",
+                        labelStyle: textStyle,
+                        hintText: "Ví dụ: 10%",
+                        fillColor: Colors.white,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(10.0),
+                          borderSide: new BorderSide(),
+                        ),
+                        prefixIcon: Icon(Icons.rate_review))),
               ),
               Container(
                 margin: const EdgeInsets.only(top: 20.0),
@@ -82,20 +110,26 @@ class _HomeScreenWidgetState extends State<HomeScreenWidget> {
                   children: <Widget>[
                     Expanded(
                       child: TextField(
+                          controller: termController,
+                          style: textStyle,
+                          keyboardType: TextInputType.number,
                           decoration: new InputDecoration(
-                        labelText: "Term",
-                        fillColor: Colors.white,
-                        border: new OutlineInputBorder(
-                          borderRadius: new BorderRadius.circular(10.0),
-                          borderSide: new BorderSide(),
-                        ),
-                      )),
+                              labelText: "Kỳ hạn",
+                              hintText: "10 năm",
+                              labelStyle: textStyle,
+                              fillColor: Colors.white,
+                              border: new OutlineInputBorder(
+                                borderRadius: new BorderRadius.circular(10.0),
+                                borderSide: new BorderSide(),
+                              ),
+                              prefixIcon: Icon(Icons.date_range))),
                     ),
                     Container(
                       width: 10,
                     ),
                     Expanded(
                       child: DropdownButton<String>(
+                        style: textStyle,
                         items: _currencies.map((String value) {
                           return DropdownMenuItem<String>(
                             value: value,
@@ -103,9 +137,7 @@ class _HomeScreenWidgetState extends State<HomeScreenWidget> {
                           );
                         }).toList(),
                         onChanged: (String newValueSelected) {
-                          setState(() {
-                            this._currentItemSelected = newValueSelected;
-                          });
+                          _onDropDownItemSelected(newValueSelected);
                         },
                         value: _currentItemSelected,
                       ),
@@ -121,10 +153,17 @@ class _HomeScreenWidgetState extends State<HomeScreenWidget> {
                       child: Container(
                         height: 50,
                         child: RaisedButton(
-                          child: Text("Calculate"),
+                          child: Text(
+                            "Tính",
+                            style: TextStyle(fontSize: 20.0),
+                          ),
                           color: Colors.blue,
                           textColor: Colors.white,
-                          onPressed: () {},
+                          onPressed: () {
+                            setState(() {
+                              displayResult = _calculateInterested();
+                            });
+                          },
                         ),
                       ),
                     ),
@@ -135,10 +174,17 @@ class _HomeScreenWidgetState extends State<HomeScreenWidget> {
                       child: Container(
                         height: 50,
                         child: RaisedButton(
-                          child: Text("Reset"),
-                          color: Colors.black,
+                          child: Text(
+                            "Làm lại",
+                            style: TextStyle(fontSize: 20.0),
+                          ),
+                          color: Colors.red,
                           textColor: Colors.white,
-                          onPressed: () {},
+                          onPressed: () {
+                            setState(() {
+                              _onReseted();
+                            });
+                          },
                         ),
                       ),
                     ),
@@ -147,12 +193,52 @@ class _HomeScreenWidgetState extends State<HomeScreenWidget> {
               ),
               Container(
                 margin: const EdgeInsets.only(top: 20.0),
-                child: Text("Result will show here!"),
+                child: Text(
+                  "${displayResult}",
+                  style: TextStyle(fontSize: 20.0),
+                ),
               ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  String _calculateInterested() {
+    double principal = double.parse(principalController.text);
+    double roi = double.parse(roiController.text);
+    double term = double.parse(termController.text);
+
+    double totalAmountPayable = _logicCalculated(principal, roi, term);
+
+    MoneyFormatterOutput fo = _formatCurrency(totalAmountPayable);
+
+    return "Sau ${term.toString()} năm, bạn sẽ lãi ${fo.withoutFractionDigits} ${_currentItemSelected}";
+  }
+
+  double _logicCalculated(double principal, double roi, double term) {
+    double totalAmountPayable = principal + (principal * roi * term) / 100;
+    return totalAmountPayable;
+  }
+
+  MoneyFormatterOutput _formatCurrency(double totalAmountPayable) {
+    MoneyFormatterOutput fo =
+        FlutterMoneyFormatter(amount: totalAmountPayable).output;
+    return fo;
+  }
+
+  void _onDropDownItemSelected(String newValueSelected) {
+    setState(() {
+      this._currentItemSelected = newValueSelected;
+    });
+  }
+
+  void _onReseted() {
+    principalController.clear();
+    roiController.clear();
+    termController.clear();
+    displayResult = "";
+    _currentItemSelected = _currencies[0]; // Default: VietNamDong
   }
 }
